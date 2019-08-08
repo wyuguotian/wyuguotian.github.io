d7e469050190485b4d55d9461d7095f9667fa2b4
wyuguotian <53576623+wyuguotian@users.noreply.github.com>
2019-08-08 20:25:22 +0800

    Update mightygumball.js

--- mightygumball.js
+++ mightygumball.js
@@ -7,7 +7,7 @@
 window.onload = init;
 
 function init() {
-	getSales();
+//	getSales();
 }
 
 //
@@ -15,29 +15,15 @@ function init() {
 // and Chrome) you can check progress and check for the "load" event with the
 // onload event handler instead of checking the onreadystatechange
 //
-function getSales() {
-	// change the URL to match the location where you
-	// put the sales.json file
-    	var url = "https://gumball.wickedlysmart.com";
-	var request = new XMLHttpRequest();
-	request.open("GET", url);
-	request.onload = function() {
-		if (request.status == 200) {
-			updateSales(request.responseText);
-		}
-	};
-	request.send(null);
-}
 
-function updateSales(responseText) {
-	var salesDiv = document.getElementById("sales");
-	var sales = JSON.parse(responseText);
-	for (var i = 0; i < sales.length; i++) {
-		var sale = sales[i];
-		var div = document.createElement("div");
-		div.setAttribute("class", "saleItem");
-		div.innerHTML = sale.name + " sold " + sale.sales + " gumballs";
-		salesDiv.appendChild(div);
+
+function updateSales(sales) {
+    	var salesDiv = document.getElementById("sales");        
+	for (var i = 0; i < sales.length; i++) {        
+		var sale = sales[i];        
+		var div = document.createElement("div");        
+		div.setattribute("class", "saleItem");        
+		div.innerHTML = sale.name + " sold " + sale.sales + " gumballs";        salesDiv.appendChild(div);    
 	}
 }
 
